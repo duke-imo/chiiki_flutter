11c2214d8b97dce6896c43581479c59fbae2eb01
duke-imo <kato1337826577@gmail.com>
2024-02-22 12:16:36 +0900

    username upload enabled

--- lib/database.dart
+++ lib/database.dart
@@ -5,26 +5,19 @@ class Post {
   final String username;
 
   Post({
-    required this.text, 
+    required this.text,
     //required this.imageBytes,
     required this.date,
     required this.username,
   });
 }
 
-List<Post> posts = [//投稿を格納するリスト
-  Post(text: "テスト", date: DateTime.now(), username: "Cocon")
-];
+//投稿を格納するリストで、初期値として1つの投稿を持つ
+List<Post> posts = [Post(text: "テスト", date: DateTime.now(), username: "Cocon")];
 
-String createPost(String text, String date, String username) {//投稿を追加する関数
+void createPost(String text, DateTime date, String username) {
   //   if (text.isEmpty || date.isEmpty || username.isEmpty) {
   //   throw ArgumentError("引数が不足しています");
   // }
-
-  posts.add(Post(
-    text: text,
-    date: DateTime.now(),
-    username: username
-  ));
-  return "投稿しました";
+  posts.add(Post(text: text, date: date, username: username));
 }
--- lib/post.dart
+++ lib/post.dart
@@ -4,14 +4,16 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_chiiki/main.dart';
 
 class PostPage extends StatefulWidget {
+  const PostPage({super.key});
 
   @override
   _PostPagePageState createState() => _PostPagePageState();
 }
 
 class _PostPagePageState extends State<PostPage> {
-
-  TextEditingController _textEditingController = TextEditingController();
+  final TextEditingController _textEditingController = TextEditingController();
+  final TextEditingController _usernameEditingController =
+      TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -19,37 +21,59 @@ class _PostPagePageState extends State<PostPage> {
       appBar: AppBar(
         title: const Text("投稿画面"),
       ),
-      body: Center(
-        child: TextField(
-          controller: _textEditingController,
-          enabled: true,
-          maxLength: 50, 
-          style: const TextStyle(color: Colors.black),
-          obscureText: false,
-          maxLines:1 ,
-          decoration: const InputDecoration(
-            icon: Icon(Icons.speaker_notes),
-            hintText: '投稿内容を記載します',
-            labelText: '内容 * ',
-          ),
-        ),
-      ),
+      body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                TextField(
+                  controller: _textEditingController,
+                  enabled: true,
+                  maxLength: 50,
+                  style: const TextStyle(color: Colors.black),
+                  obscureText: false,
+                  maxLines: 1,
+                  decoration: const InputDecoration(
+                    icon: Icon(Icons.speaker_notes),
+                    hintText: '投稿内容を記載します',
+                    labelText: '内容 * ',
+                  ),
+                ),
+                TextField(
+                  controller: _usernameEditingController,
+                  enabled: true,
+                  maxLength: 50,
+                  style: const TextStyle(color: Colors.black),
+                  obscureText: false,
+                  maxLines: 1,
+                  decoration: const InputDecoration(
+                    icon: Icon(Icons.speaker_notes),
+                    hintText: 'ユーザ名を入力してください',
+                    labelText: 'ユーザ名 * ',
+                  ),
+                ),
+              ])),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
           final prefs = await SharedPreferences.getInstance();
+
           prefs.setString('saved_text', _textEditingController.text);
+          prefs.setString('saved_username', _usernameEditingController.text);
 
           String savedText = prefs.getString('saved_text') ?? "No Data";
+          String savedUsername = prefs.getString('saved_username') ?? "No Data";
 
-          createPost(savedText, DateTime.now().toString(), "kato");
+          createPost(savedText, DateTime.now(), savedUsername);
 
           Navigator.push(
             context,
-            MaterialPageRoute(builder: (context) => const MyHomePage(title: 'chiiki_flutter')),
+            MaterialPageRoute(
+                builder: (context) =>
+                    const MyHomePage(title: 'chiiki_flutter')),
           );
         },
-          child: const Icon(Icons.add),
-        ),
-      );
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
